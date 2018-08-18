bed313eb82d6cc03452feda06e7754af067e2869
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-08-18 10:33:06 +0530

    Create guvi63.java

---
+++ guvi63.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n1=9,n2=2;
+        int n=n1+n2;
+if(n%2==0)
+{
+System.out.println("given number is even");
+}
+else
+{
+System.out.println("given number is odd");
+}
+	}
+}
+	
